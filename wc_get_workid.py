89a6dbc63c587ccbe385b18f01be52b264d11704
Sam Kome <skome@canecreekstudio.org>
2018-04-23 11:25:06 -0700

    rdflib working code achieved!

--- wc_get_workid.py
+++ wc_get_workid.py
@@ -5,11 +5,14 @@ import requests
 import csv
 import json
 
-urlStub = "http://www.worldcat.org/oclc/" # API URL
+# retrieving data from the linked data...thing. API?
+url_stub_oclcnum = 'http://www.worldcat.org/oclc/' # API URL for oclcnum
+url_stub_workid = 'http://www.worldcat.org/work/' # API URL for workid
 headers = {"Accept":"application/ld+json"} # a default format for the OCLC data 
 
 """ 
-Usage: provide a file of oclc numbers as the first parameter and a filename to receive results as a second parameter. 
+Usage: provide a file of oclc numbers or workids as the first parameter and a 
+filename to receive results as a second parameter. 
 Results will be the workids associated with the oclcnumbers
 %prog [inputfile] [outputfile]]
 """
@@ -28,19 +31,21 @@ class codesList:
 def get_DOM(query_url, headers):
     """Accept a URL, retrieve the contents at the URL, return the values. """
     #to do: 
-    q = requests.get(query_url, headers=headers) 
+    q = requests.get(query_url, headers=headers)
+    print(q.text)
     dom = q.text
     return True, dom
 
 def search(lCodes):
     """Accept a list of item (oclcnum) codes, loop through the list,
-    search Worldcat, return results in a (dictionary?)"""
+    search Worldcat, return workids. Loop through workids, get oclcnums
+    Loop through oclcnums, look for ebook bookFormat"""
     for oclcSym in lCodes:
         found = False
         print('code: ',oclcSym)
         #query = urlStub+oclcSym+'?wskey='+wsKey+"&oclcsymbol= \
         #"+urllib.quote(libs)
-        query = urlStub+oclcSym
+        query = url_stub_oclcnum+oclcSym
         #print query,        
         found, dom = get_DOM(query, headers)
         #to do: allow user to choose non-default header 
@@ -49,17 +54,39 @@ def search(lCodes):
             try: 
                 graph = json.loads(dom)['@graph']
                 for key in graph:
-                    try:
-                        print(key['@type'], '\n\t',key['@id'])
-                    except NoneType:
-                        print('no type?')
+                    if 'exampleOfWork' in key:
+                        #retrieve oclcnums
+                        success, dom2 = get_DOM(key['exampleOfWork'], headers)
+                        if success:
+                            print('workID',key['exampleOfWork'])
+                            try: 
+                                success, dom2 = get_DOM(key['exampleOfWork'], headers)
+                                graph = json.loads(dom2)['@graph']
+                                for key in graph:
+                                    if 'workExample' in key:
+                                        #retrieve oclcnums
+                                        #print(key['workExample'])
+                                        for ocn_url in key['workExample']:
+                                            success, dom3 = get_DOM(ocn_url, headers)
+                                            if success:
+                                                graph = json.loads(dom3)['@graph']
+                                                for key in graph:
+                                                    if 'bookFormat' in key:
+                                                        print(ocn_url,key['bookFormat'])
+                            except:
+                                print('ouch')
+#                    try:
+#                        print(key['@type'], '\n\t',key['@id'])
+#                    except NoneType:
+#                        print('no type?')
             except NameError:
                 #something besides @graph or @type? Whaaat
                 print("can't deal with these data: ", dom)
                 #raise
+            except KeyError:
+                print(key.keys())
         else:
             print('{},{}').format(str(oclcSym), " lookup failed.")
-
 if __name__ == "__main__":
     fileIn = sys.argv[1]
     fileOut = sys.argv[2]    
---
+++ wc_get_workid_rdf-stlye.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python
+import csv
+import sys
+import requests
+from rdflib import Graph, URIRef, Literal, BNode
+
+# retrieving data from the linked data...thing. API?
+url_stub_oclcnum = 'http://www.worldcat.org/oclc/' # API URL for oclcnum
+url_stub_workid = 'http://www.worldcat.org/work/' # API URL for workid
+rdf_data_format = 'application/rdf+xml'
+json_data_format = 'application/ld+json'
+headers_json = {'Accept' : json_data_format} # a default format for the OCLC data
+headers_rdf = {'Accept': rdf_data_format}
+pred_workid = URIRef(u'http://schema.org/exampleOfWork')
+pred_oclcnum = URIRef(u'http://schema.org/workExample')
+pred_format = URIRef('http://schema.org/bookFormat')
+
+
+""" 
+Usage: provide a file of oclc numbers or workids as the first parameter and a 
+filename to receive results as a second parameter. 
+Results will be the workids associated with the oclcnumbers
+%prog [inputfile] [outputfile]]
+"""
+
+class codesList:
+    """Accept a plaintext file, return a list of the lines stripped of leading 
+    or trailing whitespace """
+    def __init__(self, fh):
+        self.codeFile = fh
+    def listed(self):
+        lines=[]
+        for line in self.codeFile.readlines():
+            lines.append(line.strip())
+        return lines
+
+def get_DOM(query_url, headers):
+    """Accept a URL, retrieve the contents at the URL, return the values. """
+    #to do: 
+    q = requests.get(query_url, headers=headers)
+    dom = q.text
+    return dom
+
+def get_work_IDs(lCodes):
+    """Accept a list of item (oclcnum) codes, loop through the list,
+    search Worldcat, return workids. """
+    work_ids = []
+    #Get the workids
+    for oclcSym in lCodes:
+        uri = url_stub_oclcnum+oclcSym
+        print(uri)
+        rdf_triple_data = get_DOM(uri,headers_rdf)
+        graph = Graph()
+        try:
+            graph.parse(data=rdf_triple_data, format=rdf_data_format)
+            for oclcnum, workid in graph.subject_objects(pred_workid):
+                print(oclcnum.split('/')[-1],workid.split('/')[-1])
+                work_ids.append(workid.split('/')[-1])
+        except TypeError as te:
+            print("TypeError ",uri, te)
+            print(rdf_triple_data)
+    return work_ids
+
+def getOCLC_ISBN_Nums(workids):
+    """Loop through workids, get oclcnums
+    Loop through oclcnums, look for ebook bookFormat"""
+    oclc_nums = []
+    print(workids)
+    for workid in workids:
+        uri = url_stub_workid+workid
+        rdf_triple_data = get_DOM(uri,headers_rdf)
+        graph = Graph()
+        graph.parse(data=rdf_triple_data, format='application/rdf+xml')
+        for oclcnum, isbn in graph.subject_objects(pred_workid):
+            #print(oclcnum.split('/')[-1], isbn.split('/')[-1])
+            oclc_nums.append([oclcnum.split('/')[-1], isbn.split('/')[-1]])
+    return oclc_nums        
+        
+if __name__ == "__main__":
+    fileIn = sys.argv[1]
+    fileOut = sys.argv[2]    
+    lCodes=[]
+    with open(fileIn, 'r') as f_oclc_nums, open(fileOut, 'w', newline='') as \
+    f_out:
+        csv_writer = csv.writer(f_out)
+        oList = codesList(f_oclc_nums) #read all the OCLC numbers into a list
+        lCodes = oList.listed()
+        workids = get_work_IDs(lCodes)
+        oclcnums = getOCLC_ISBN_Nums(workids)
+        #csv_writer.writerows(results)
+        #print(results)
+        #write results to a csv(?) file
+
