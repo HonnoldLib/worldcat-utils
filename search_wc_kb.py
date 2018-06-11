034272b50209cfefdbf0a70d785dfb436aa22300
Sam Kome <skome@canecreekstudio.org>
2018-06-11 14:17:58 -0700

    refactor: getURL

--- search_wc_kb.py
+++ search_wc_kb.py
@@ -9,32 +9,37 @@ import requests
 
 json_data_format = 'application/json'
 headers_json = {'Accept' : json_data_format}
-institution = '5640'
-#start_pos = 1
-max_results = 1000
-items_per_page = 50
-content_type = 'ebook'
-wskey='xAW2qDuioeBKBdIPHuUrBNPGqk78NmUsaB8LP2BERjpClphypY9Nn92E2tesAgCvmY0G6bbM5DzaQSPS'
+INST_ID = '5640'
+MAX_RES = 100
+ITEMS_PP = 50
+CONTENT_TYPE = 'ebook'
+URL_STUB = 'http://worldcat.org/webservices/kb/rest/entries/search?'
+WSKEY='xAW2qDuioeBKBdIPHuUrBNPGqk78NmUsaB8LP2BERjpClphypY9Nn92E2tesAgCvmY0G6bbM5DzaQSPS'
 info =[]
 
-with open('delme.txt', 'w', encoding='utf-8') as result_file:
-    for start_i in range(0,max_results,items_per_page):
-        #build a URL with updated values for start-index (and others as wanted)
-        url = 'http://worldcat.org/webservices/kb/rest/entries/search?' \
-        'institution_id={}&start-index={}&max-results={}&itemsPerPage={}' \
-        '&content={}&wskey={}'.format(institution,start_i,max_results,
-                  items_per_page,content_type,wskey)
-        q = requests.get(url,headers=headers_json)
-        results = json.loads(q.text)
-        for i, entry in enumerate(results['entries']):
-            try:
-                info.append('{}{}{}'.format(str(entry['title']).strip(),'|',
-                            entry['kb:provider_name']))
-            except AttributeError as ae:
-                print(ae, entry)
-            except KeyError as ke:
-                print(ke, entry)
-    result_file.write('\n'.join(info))
-        #    for e, key in enumerate(entry.keys()):
-        #        print('{}:{}: {}'.format(i,key,entry[key]))
-            #print('{}: {}{}'.format(str(i), entry['title'],'\n'))
+def getURL(inst_id,start_pos):
+    url = URL_STUB+'institution_id={}&start-index={}&max-results={}&itemsPerPage={}' \
+            '&content={}&wskey={}'.format(inst_id,start_pos,MAX_RES,
+                      ITEMS_PP,CONTENT_TYPE,WSKEY)
+    return url
+
+if __name__ == '__main__':
+    with open('delme.txt', 'w', encoding='utf-8') as result_file:
+        for start_i in range(0,MAX_RES,ITEMS_PP):
+            #build a URL with updated values for start-index (and others as wanted)
+            institution_id = INST_ID
+            url = getURL(institution_id, start_i)
+            q = requests.get(url,headers=headers_json)
+            results = json.loads(q.text)
+            for i, entry in enumerate(results['entries']):
+                try:
+                    info.append('{}{}{}'.format(str(entry['title']).strip(),'|',
+                                entry['kb:provider_name']))
+                except AttributeError as ae:
+                    print(ae, entry)
+                except KeyError as ke:
+                    print(ke, entry)
+        result_file.write('\n'.join(info))
+            #    for e, key in enumerate(entry.keys()):
+            #        print('{}:{}: {}'.format(i,key,entry[key]))
+                #print('{}: {}{}'.format(str(i), entry['title'],'\n'))
