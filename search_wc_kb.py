a9306e67deb1aa1cd8d21b4cda953f57d3a31728
Sam Kome <skome@canecreekstudio.org>
2018-06-15 14:37:46 -0700

    Added config file

--- search_wc_kb.py
+++ search_wc_kb.py
@@ -15,35 +15,40 @@ INST_ID = '5640'
 MAX_RES = 100
 ITEMS_PP = 50
 CONTENT_TYPE = 'ebook'
-URL_STUB = 'http://worldcat.org/webservices/kb/rest/entries/search?'
+URL_STUB_ENTRIES = 'http://worldcat.org/webservices/kb/rest/entries/search?'
+URL_STUB_COLLECTIONS = 'http://worldcat.org/webservices/kb/rest/collections/search?q='
 WSKEY='xAW2qDuioeBKBdIPHuUrBNPGqk78NmUsaB8LP2BERjpClphypY9Nn92E2tesAgCvmY0G6bbM5DzaQSPS'
 info =[]
 
 def print_status(numcodes, totalNum, msg): #progress indicator
-    """status printing utility for long-running scripts"""
+    """print status message for long-running scripts"""
     print('{} {} {}\r'.format(numcodes, totalNum, msg), end='\r'),
     sys.stdout.flush() 
     
-def get_URL(start_pos):
-    url = URL_STUB+'institution_id={}&start-index={}&max-results={}&itemsPerPage={}' \
+def get_URL(start_pos, url):
+    """build a URL string that includes the starting position in the search result."""
+    url += 'institution_id={}&start-index={}&max-results={}&itemsPerPage={}' \
             '&content={}&wskey={}'.format(INST_ID,start_pos,MAX_RES,
                       ITEMS_PP,CONTENT_TYPE,WSKEY)
     return url
 
 def get_KB_Data(url):
+    """submit a URL request and return the result (or error message) """
     try:
         q = requests.get(url, headers=HEADERS_JSON)
         return q
     except:
-        print('error in request: {}'.format(sys.exc_info()[0]))
+        #print('error in request: {}'.format(sys.exc_info()[0]))
+        logging.exception('unexpected error getting data at url')
         return ''
     
 def parse_json_to_dict(text_data):
+    """parse json-containing text, return a python dictionary"""
     try:
         results = json.loads(text_data)
         return results
     except:
-        print('error parsing json result: {}'.format(sys.exc_info()[0]))
+        #print('error parsing json result: {}'.format(sys.exc_info()[0]))
         logging.exception('unexpected error parsing json result')
         return {}        
 
@@ -51,7 +56,7 @@ if __name__ == '__main__':
     with open('delme.txt', 'w', encoding='utf-8') as result_file:
         for start_i in range(0,MAX_RES,ITEMS_PP):
             #build a URL with updated values for start-index (and others as wanted)
-            url = get_URL(start_i)
+            url = get_URL(start_i,URL_STUB_ENTRIES) #use for Entries 
             q = get_KB_Data(url)
             results = parse_json_to_dict(q.text)
             #add results to a dictionary, and finally to a file
